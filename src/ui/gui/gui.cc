64411404fbab55c40a0b68c12ed5a3b01c16a71f
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2022-04-30 13:40:13 +0530

    fixed a minor bug

--- src/ui/gui/gui.cc
+++ src/ui/gui/gui.cc
@@ -5,8 +5,8 @@ int main(int argc, char **argv)
 	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
 	builder = Gtk::Builder::create_from_file("./design.glade");
 
-	init_database();
 	init_variables();
+	init_database();
 	main_window->set_default_size(800, 600);
 	app->run(*main_window);
 
